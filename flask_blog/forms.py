4576c4c6e3bd96db521918af20c4d961ad476b9c
Nkosingiphilee <150391714+Nkosingiphilee@users.noreply.github.com>
2024-03-22 22:10:19 +0200

    Add files via upload

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,6 +1,8 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
-from wtforms.validators import DataRequired, length, Email, EqualTo
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import FileField, StringField, PasswordField, SubmitField, BooleanField, TextAreaField, ValidationError
+from wtforms.validators import DataRequired, length, Email, EqualTo,Length
 
 from flask_blog.models import User
 
@@ -34,3 +36,28 @@ class LoginForm(FlaskForm):
                              validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+            
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
